439bc9e177773c83ffefb8dec1f8f72b34e142ec
mancku <adria.lopez.ortega@gmail.com>
2025-11-01 00:11:56 +0100

    Renamed

--- EasyMapper/EasyMapper.cs
+++ EasyMapper/EasyMapper.cs
@@ -1,46 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-
-namespace EasyMapper
+﻿namespace EasyMapper
 {
-    public class PropertyMapper<TSource, TTarget> where TTarget : class
+    using System;
+    using System.Collections.Generic;
+
+    public class EasyMapper<TSource, TTarget> where TTarget : class
     {
         private readonly TSource _source;
         private readonly TTarget _target;
         private Action<TTarget, TSource>? _customAfterMap;
         private readonly List<string> _ignoreProperties;
 
-        public PropertyMapper(TSource source, TTarget target)
+        public EasyMapper(TSource source, TTarget target)
         {
             _source = source;
             _target = target;
             _ignoreProperties = new List<string>();
         }
 
-        public static PropertyMapper<TSource, TTarget> Create(TSource source, TTarget target)
+        public static EasyMapper<TSource, TTarget> Create(TSource source, TTarget target)
         {
-            return new PropertyMapper<TSource, TTarget>(source, target);
+            return new EasyMapper<TSource, TTarget>(source, target);
         }
 
-        public PropertyMapper<TSource, TTarget> WithCustomAfterMap(Action<TTarget, TSource> customAfterMap)
+        public EasyMapper<TSource, TTarget> WithCustomAfterMap(Action<TTarget, TSource> customAfterMap)
         {
             _customAfterMap = customAfterMap;
             return this;
         }
 
-        public PropertyMapper<TSource, TTarget> IgnoreProperties(IEnumerable<string> ignoreProperties)
+        public EasyMapper<TSource, TTarget> IgnoreProperties(IEnumerable<string> ignoreProperties)
         {
             _ignoreProperties.AddRange(ignoreProperties);
             return this;
         }
 
-        public PropertyMapper<TSource, TTarget> IgnoreProperties(params string[] ignoreProperties)
+        public EasyMapper<TSource, TTarget> IgnoreProperties(params string[] ignoreProperties)
         {
             _ignoreProperties.AddRange(ignoreProperties);
             return this;
         }
 
-        public PropertyMapper<TSource, TTarget> IgnoreProperty(string propertyName)
+        public EasyMapper<TSource, TTarget> IgnoreProperty(string propertyName)
         {
             _ignoreProperties.Add(propertyName);
             return this;
